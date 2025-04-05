f821a3df09d41af02406177d2dd28ad354e34b3d
Jamie Howell <69440188+Totally-A-Boar@users.noreply.github.com>
2025-04-05 13:26:17 -0400

    Add annotations in wWinMain entry

--- src/Clock/Main.c
+++ src/Clock/Main.c
@@ -4,7 +4,7 @@
 #include "NTPClient.h"
 #include "AboutWindow.h"
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
 	UNREFERENCED_PARAMETER(hPrevInstance); // https://learn.microsoft.com/en-us/archive/msdn-magazine/2005/may/c-at-work-unreferenced-parameters-adding-task-bar-commands
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
